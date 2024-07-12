1305ba5355890e9368207466f9b4e9cc129ede8a
Vitalis Ian <viterlis254@gmail.com>
2024-07-12 12:17:10 +0300

    adds index.js

--- index.js
+++ index.js
@@ -1,23 +1,21 @@
-// Add your code here
 function submitData(userName, userEmail) {
-    const userData = {
-      name: steve,
-      email: steve@steve.com
-    }
-  
-    const submitData = {
-      method: 'POST',
-      headers: {
-        'Content-Type':'application/json',
-        Accept: 'application/json'
-      },
-      body: JSON.stringify(userData)
-    }
-  
-    return fetch('http://localhost:3000/users', submitData)
-      .then(response => response.json())
-      .then(data => document.body.innerHTML = data.id)
-      .catch(error => document.body.innerHTML = error.message)
+  const userInfo = {
+    name: userName,
+    email: userEmail
   }
 
-  
+  const configUser = {
+    method: 'POST',
+    headers: {
+      'Content-Type':'application/json',
+      Accept: 'application/json'
+    },
+    body: JSON.stringify(userInfo)
+  }
+
+  return fetch('http://localhost:3000/users', configUser)
+    .then(response => response.json())
+    .then(data => document.body.innerHTML = data.id)
+    .catch(error => document.body.innerHTML = error.message)
+}
+
